b215d7ca5d66265d90d87e9a36f0735d1a7b1eb8
emirhankudun-ux <emirhankudun@gmail.com>
2026-06-12 11:25:56 +0300

    Add Apple native shell run handoff

--- packages/seis_platform_swift/Sources/SeisAppleNativeShell/App/SeisAppleNativeShellApp.swift
+++ packages/seis_platform_swift/Sources/SeisAppleNativeShell/App/SeisAppleNativeShellApp.swift
@@ -1,8 +1,23 @@
 import SeisPlatformKit
 import SwiftUI
 
+#if os(macOS)
+import AppKit
+
+final class SeisAppleNativeShellAppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        NSApp.setActivationPolicy(.regular)
+        NSApp.activate(ignoringOtherApps: true)
+    }
+}
+#endif
+
 @main
 struct SeisAppleNativeShellApp: App {
+    #if os(macOS)
+    @NSApplicationDelegateAdaptor(SeisAppleNativeShellAppDelegate.self) private var appDelegate
+    #endif
+
     var body: some Scene {
         WindowGroup("SEIS Apple Native") {
             AppleContinuationWindow()
