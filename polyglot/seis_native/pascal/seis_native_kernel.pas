9eda68c17d275c0588b855f203c159d2a0762afe
emirhankudun-ux <emirhankudun@gmail.com>
2026-07-05 16:27:39 +0300

    Add Pascal native roadmap constants

---
+++ polyglot/seis_native/pascal/seis_native_kernel.pas
@@ -0,0 +1,22 @@
+unit SeisNativeKernel;
+
+interface
+
+type
+  TNativeRoadmapItem = record
+    Lane: string;
+    Score: Integer;
+    LanguageName: string;
+  end;
+
+const
+  AppleFirstLane = 'Apple First';
+  DataAILane = 'Data AI';
+  SystemsLane = 'Systems';
+  AndroidLane = 'Android';
+  WindowsLane = 'Windows';
+  InfrastructureLane = 'Infrastructure';
+
+implementation
+
+end.
