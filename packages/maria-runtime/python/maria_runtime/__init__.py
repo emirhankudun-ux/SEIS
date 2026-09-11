988e61812f12b2bd86ace9678524d77b3a41ce84
emirhankudun-ux <emirhankudun@gmail.com>
2026-09-11 11:50:16 +0300

    feat(maria): export recovery reconciliation API

--- packages/maria-runtime/python/maria_runtime/__init__.py
+++ packages/maria-runtime/python/maria_runtime/__init__.py
@@ -64,6 +64,12 @@ from .models import ModelRegistry, ModelSpec
 from .permissions import ActionClass, PermissionDecision, PermissionEngine
 from .projects import ProjectProfile, ProjectRegistry, WorkMode, default_project_registry
 from .providers import ProviderRegistry, ProviderSpec, ProviderStatus, default_provider_registry
+from .recovery_reconciliation import (
+    RecoveryAnchor,
+    RecoveryReconciler,
+    RecoveryReconciliationAssessment,
+    RecoveryReconciliationDisposition,
+)
 from .registry import CapabilityRegistry, ToolSpec, ToolStatus
 from .routing import ModelRouteDecision, ModelRouter
 from .safety import CommandPolicy
@@ -106,7 +112,8 @@ __all__ = [
     "ModelWorkInput", "ModelWorkStepBinding", "ModelWorkStepRunner", "MultiStepWorkRouter", "OllamaModelWorkAdapter",
     "PermissionDecision", "PermissionEngine", "ProbeHealthSummary", "ProbeObservation", "ProbeOutcome",
     "ProjectContextEngine", "ProjectContextRetriever", "ProjectProfile", "ProjectRegistry", "PromptCache",
-    "ProviderRegistry", "ProviderSpec", "ProviderStatus", "RecoveryAssessment", "RecoveryDisposition", "RouteDecision",
+    "ProviderRegistry", "ProviderSpec", "ProviderStatus", "RecoveryAnchor", "RecoveryAssessment", "RecoveryDisposition",
+    "RecoveryReconciler", "RecoveryReconciliationAssessment", "RecoveryReconciliationDisposition", "RouteDecision",
     "RouteKind", "RuntimeProbeState", "RuntimeProbeStatus", "TextNormalizer", "ToolSpec", "ToolStatus",
     "UnifiedCapabilityRouter", "WorkMode", "WorkPlanCheckpoint", "WorkPlanExecutionResult", "WorkPlanExecutor",
     "WorkRoutePlan", "WorkRouteStep", "WorkStepExecutionEvidence", "WorkStepExecutionPolicy",
